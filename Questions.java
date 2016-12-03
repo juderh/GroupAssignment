fc949c9786326f68a03abe27558dcdb244f521ce
juderh <jude_hall@ymail.com>
2016-12-02 19:55:04 -0700

    Update Questions.java
    added new questions! I'll send the rest tomorrow

--- Questions.java
+++ Questions.java
@@ -8,29 +8,29 @@ public class Questions {
 		return questions[index];
 	}
 
-	private String[] questions = {
-			String.format("Which one of these personality traits best describes you? %n"
-					+ " 1 - Courage %n 2 - Dependability %n 3 - Responsiveness %n 4 - Loyalty %n 5 - Exuberance %n"
-					+ " 6 - Conscientiousness %n 7 - Charm %n 8 - Idealism %n 9 - Optimism %n 10 - Steadiness %n"
-					+ " 11- Friendliness %n 12 - Compassion"),
-			// 1.Aries 2.Taurus 3.Gemini 4.Cancer 5.Leo 6.Virgo 7.Leo 8.Scorpio
-			// 9.Sagittarius 10.Capricorn
+			String.format("Which one of these personality traits best describes you? %n 1 – Courageous%n 2 – Dependable%n 3 – Responsive%n"
+					+ "4 – Loyal%n 5 – Exuberance%n 6 – Conscientiousness%n 7 - Charming%n8 – Idealistic%n 9 – Optimistic%n 10 – Steady" 
+					+ "11 – Friendly%n 12 – Compassionate%n"), 
+			// 1.Aries 2.Taurus 3.Gemini 4.Cancer 5.Leo 6.Virgo 7.Leo 8.Scorpio 9.Sagittarius 10.Capricorn 
 			// 11. Aquarius 12. Pisces
-
-			String.format("Be honest with yourself. Which one of these negative traits best desribes you? %n"
-					+ " 1 - Manipulative %n 2 - Cold %n 3 - Pessimistic %n 4 - Impatient %n 5 - Secretive %n 6 - "
-					+ "Indecisive %n 7 - Petty %n 8 - Bossy %n 9 - CLingy %n 10 - Sarcastic %n 11 - Stubborn %n "
-					+ "12 - Impulsive"),
-			// 1.Pisces 2.Aquarius 3.Capricorn 4.Sagittarius 5.Scorpio 6.Libra
-			// 7.Virgo 8.Leo 9.Cancer
-			// 10.Gemini 11.Taurus 12.Aries
-
-			String.format("Which one of these is most likely to cause you stress? %n"
-					+ " 1 - You forgot to slow down, and take a lazy day%n 2 - Not enough time to finish a "
-					+ "task just the way you want%n 3 - Someone spells or mispronounces a word%n 4 - "
-					+ "Being brushed off or offended by someone%n 5 - Not being in complete control of "
-					+ "a situation%n 6 - You are over thinking something%n 7 - Not being treated fairly by the "
-					+ "people around you%n 8 - No one is taking you seriously%n 9 - Being stuck in traffic%n"
-					+ "10 - You demanded to much from yourself%n 11 - something that would make an aquarius "
-					+ "stressed out%n 12 - Public speaking"), };
+			
+			String.format("What about these personality traits? Be honest with yourself.%n 1 – Impulsive%n 2 – Stubborn%n 3 – Sarcastic%n"
+					+ "4 – Clingy%n 5 – Bossy%n 6 – Petty%n 7 – Indecisive%n 8 – Secretive%n 9 – Impatient%n 10 – Pessimistic%n 11 – Cold%n"
+					+ "12 – Manipulative"),
+			// 1.Aries 2.Taurus 3.Gemini 4.Cancer 5.Leo 6.Virgo 7.Leo 8.Scorpio 9.Sagittarius 10.Capricorn 
+			// 11. Aquarius 12. Pisces
+			
+			
+			String.format("Choose your dream job from this list.%n 1 – Entrepreneur%n 2 – Designer%n 3 – Architect%n 4 – CEO%n 5 – Musician%n"
+					+ "6 – Teacher%n 7 – Diplomat%n 8 – Detective%n 9 -  Public Relations%n 10 -  Scientist%n 11 - Artist%n 12 – Nurse%n"
+					+ "13 – My dream job isn't on this list!"),
+			// 1.Aries 2.Taurus 3.Gemini 4.Cancer 5.Leo 6.Virgo 7.Leo 8.Scorpio 9.Sagittarius 10.Capricorn 
+			// 11. Aquarius 12. Pisces 13.conversation
+			
+		    String.format("What do people do that really makes you mad?%n 1 – People who take forever to text back%n 2 – People who don't clean up after themselves%n"
+                    + "3 -  People who interrupt you when you're talking%n 4 -  People who are forgetful%n 5 – People who don't pay enough attention%n"
+                    + "6 – People complain to them often%n 7 – People who don't like loud music%n 8 -  People who aren't open books%n"
+                    + "9 -  People who don't speak their mind%n 10 – People who want to borrow things%n 11 – People who like to tease you%n 12 – People who are sarcastic%n"),
+			// 1.Aries 2.Taurus 3.Gemini 4.Cancer 5.Leo 6.Virgo 7.Leo 8.Scorpio 9.Sagittarius 10.Capricorn 
+			// 11. Aquarius 12. Pisces
 }
